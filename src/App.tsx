47df632763acae658831e2d00376c4728e58b2e6
Pavel <braurs64@gmail.com>
2025-04-09 11:50:05 +0400

    feat: the first task is closed

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,24 @@ import { useState } from "react";
 import { Counter } from "./challenges/01-counter/Counter";
 
 function App() {
-  const [currentChallenge, setCurrentChallenge] = useState<string | null>(null);
+  const [currentChallenge, setCurrentChallenge] = useState<string | null>(
+    "counter"
+  );
   return (
     <div>
       <h1>React Challenges</h1>
 
-      <div>
-        <button onClick={() => setCurrentChallenge("counter")}>
+      <div className="nav-block">
+        <button
+          onClick={() => setCurrentChallenge("counter")}
+          className={currentChallenge !== null ? "btn-active" : ""}
+        >
           01: Counter
         </button>
-        <button onClick={() => setCurrentChallenge(null)}>Reset</button>
+
+        <button className="btn-reset" onClick={() => setCurrentChallenge(null)}>
+          Reset
+        </button>
       </div>
 
       <div>{currentChallenge === "counter" && <Counter />}</div>
--- src/challenges/01-counter/Counter.tsx
+++ src/challenges/01-counter/Counter.tsx
@@ -1,3 +1,31 @@
+import { useState } from "react";
+import "./Counter.css";
+
 export const Counter = () => {
-  return <p>Counter</p>;
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <>
+      <p>
+        Task: Create a counter with two buttons (+/-) and the ability to{" "}
+        <span
+          onClick={() => setCount(0)}
+          style={{ color: "#b22222", cursor: "pointer" }}
+        >
+          reset
+        </span>{" "}
+        the value to 0
+      </p>
+
+      <div className="count-block">
+        <button className="btn-square" onClick={() => setCount(count - 1)}>
+          &minus;
+        </button>
+        <div style={{ cursor: "default" }}>{count}</div>
+        <button className="btn-square" onClick={() => setCount(count + 1)}>
+          +
+        </button>
+      </div>
+    </>
+  );
 };
